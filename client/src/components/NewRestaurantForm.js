9c9efd6f4138e52fe51d2faf0eeb60e0cdf57621
Jeffrey Arbelaez <Jeffreyarbelaez@yahoo.com>
2023-08-10 23:31:13 -0500

    validator for new restaurant form

--- client/src/components/NewRestaurantForm.js
+++ client/src/components/NewRestaurantForm.js
@@ -1,26 +1,54 @@
 import React, { useState } from "react";
 
-function NewRestaurantForm({addRestaurantToState}) {
+function NewRestaurantForm({ addRestaurantToState }) {
+  const [name, setName] = useState("");
+  const [rating, setRating] = useState("");
 
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
 
-const [name, setName] =useState ("")
-const [rating, setRating] =useState ("")
+  const handleRatingChange = (event) => {
+    setRating(event.target.value);
+  };
 
-const newRestaurantSubmit = e => {
-  e.preventDefault ()
-  const newRestaurant = {
-    name: name,
-    rating: rating,
-  }
-  addRestaurantToState(newRestaurant)
-}
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!name || isNaN(rating) || rating < 1 || rating > 5) {
+      alert("Invalid input. Please provide a valid name and rating between 1 and 5.");
+      return;
+    }
+
+    const newRestaurant = {
+      name: name,
+      rating: parseInt(rating),
+    };
+
+    // Call the function provided by the parent component to add the restaurant
+    addRestaurantToState(newRestaurant);
+
+    // Reset form fields
+    setName("");
+    setRating("");
+  };
 
   return (
     <div className="new-restaurant-form">
       <h2>New Restaurant</h2>
-      <form onSubmit = {newRestaurantSubmit}>
-        <input onChange = {e => setName(e.target.value)}type="text" name="name" placeholder="Restaurant name" />
-        <input onChange = {e => setRating(e.target.value)}type="interger" name="rating" placeholder="Rating 1-5" />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Restaurant Name"
+          value={name}
+          onChange={handleNameChange}
+        />
+        <input
+          type="number"
+          placeholder="Rating 1-5"
+          value={rating}
+          onChange={handleRatingChange}
+        />
         <button type="submit">Add Restaurant</button>
       </form>
     </div>
--- client/src/components/Restaurants.js
+++ client/src/components/Restaurants.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from "react";
+import NewRestaurantForm from "./NewRestaurantForm";
 
 function Restaurants() {
-    return (
-        <div>Restaurants</div>
-    )
+  const [restaurants, setRestaurants] = useState([]);
+
+  const addRestaurantToState = (newRestaurant) => {
+    // Update the restaurants state with the new restaurant
+    setRestaurants([...restaurants, newRestaurant]);
+  };
+
+  return (
+    <div className="header">
+      <h1 className="center">Top Rated Restaurants</h1>
+      <NewRestaurantForm addRestaurantToState={addRestaurantToState} />
+      <div className="grid-container">
+        {restaurants.map((restaurant, index) => (
+          <div key={index} className="card">
+            <h3>{restaurant.name}</h3>
+            <p>Rating: {restaurant.rating}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Restaurants
+export default Restaurants;
